7a2538bf02382ba1420c782b288250627809c2a0
Daniel Saidi <daniel.saidi@gmail.com>
2019-05-11 23:29:21 +0200

    Define standard action blocks for keyboard actions

--- KeyboardKit/Actions/KeyboardAction.swift
+++ KeyboardKit/Actions/KeyboardAction.swift
@@ -30,3 +30,40 @@ public enum KeyboardAction: Equatable {
     switchKeyboard,
     switchToNumericKeyboard
 }
+
+public extension KeyboardAction {
+    
+    var standardInputViewControllerAction: ((UIInputViewController?) -> ())? {
+        switch self {
+        case .none: return nil
+        case .backspace: return nil
+        case .dismissKeyboard: return { controller in controller?.dismissKeyboard() }
+        case .character: return nil
+        case .image: return nil
+        case .moveCursorBack: return nil
+        case .moveCursorForward: return nil
+        case .newLine: return nil
+        case .shift: return nil
+        case .space: return nil
+        case .switchKeyboard: return nil
+        case .switchToNumericKeyboard: return nil
+        }
+    }
+    
+    var standardTextDocumentProxyAction: ((UITextDocumentProxy?) -> ())? {
+        switch self {
+        case .none: return nil
+        case .backspace: return { proxy in proxy?.deleteBackward() }
+        case .dismissKeyboard: return nil
+        case .character(let char): return { proxy in proxy?.insertText(char) }
+        case .image: return nil
+        case .moveCursorBack: return { proxy in proxy?.adjustTextPosition(byCharacterOffset: -1) }
+        case .moveCursorForward: return { proxy in proxy?.adjustTextPosition(byCharacterOffset: -1) }
+        case .newLine: return { proxy in proxy?.insertText("\n") }
+        case .shift: return nil
+        case .space: return { proxy in proxy?.insertText(" ") }
+        case .switchKeyboard: return nil
+        case .switchToNumericKeyboard: return nil
+        }
+    }
+}
