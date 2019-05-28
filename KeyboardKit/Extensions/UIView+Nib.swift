90a0915629bb89a0c2055e1eec6a8509cddbe027
Daniel Saidi <daniel.saidi@gmail.com>
2019-05-28 19:21:23 +0200

    Make more extensions public and add more unit tests

--- KeyboardKit/Extensions/UIView+Nib.swift
+++ KeyboardKit/Extensions/UIView+Nib.swift
@@ -17,21 +17,29 @@ import UIKit
 
 public extension UIView {
     
+    static var defaultNib: UINib {
+        return defaultNib()
+    }
+    
     static var defaultNibName: String {
         return String(describing: self)
+    }
+    
+    static func defaultNib(in bundle: Bundle = .main) -> UINib {
+        return UINib(nibName: defaultNibName, bundle: bundle)
     }
     
     static func fromNib(
         owner: Any,
         named nibName: String = defaultNibName,
-        in bundle: Bundle = Bundle.main) -> Self {
+        in bundle: Bundle = .main) -> Self {
         return fromNibTyped(owner: owner)
     }
     
     static func fromNibTyped<T: UIView>(
         owner: Any,
         named nibName: String = T.defaultNibName,
-        in bundle: Bundle = Bundle.main) -> T {
+        in bundle: Bundle = .main) -> T {
         let nibs = bundle.loadNibNamed(nibName, owner: owner, options: nil)
         guard let nib = nibs?[0] as? T else { fatalError("initWithDefaultNib failed") }
         return nib
