fcee7bac9448b61f53af14f9ad406d4adc4c4190
Daniel Saidi <daniel.saidi@gmail.com>
2019-05-28 10:39:23 +0200

    Add unit tests for gesture action extensions

---
+++ KeyboardKitTests/Internal Extensions/UIView+LongPressActionTests.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+LongPressActionTests.swift
+//  KeyboardKitTests
+//
+//  Created by Daniel Saidi on 2019-05-28.
+//  Copyright © 2019 Daniel Saidi. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import KeyboardKit
+
+class UIView_LongPressActionTests: QuickSpec {
+    
+    override func spec() {
+        
+        describe("adding long press action") {
+            
+            it("adds long press gesture recognizer") {
+                let view = UIView(frame: .zero)
+                expect(view.gestureRecognizers).to(beNil())
+                view.addLongPressAction {}
+                expect(view.gestureRecognizers?.count).to(equal(1))
+                expect(view.gestureRecognizers?[0] as? UILongPressGestureRecognizer).toNot(beNil())
+            }
+        }
+        
+        describe("removing long press action") {
+            
+            it("removes correct gesture recognizer") {
+                let view = UIView(frame: .zero)
+                view.addTapAction {}
+                view.addLongPressAction {}
+                expect(view.gestureRecognizers?.count).to(equal(2))
+                view.removeLongPressAction()
+                expect(view.gestureRecognizers?.count).to(equal(1))
+                expect(view.gestureRecognizers?[0] as? UITapGestureRecognizer).toNot(beNil())
+            }
+        }
+    }
+}
---
+++ KeyboardKitTests/Internal Extensions/UIView+TapActionTests.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+TapActionTests.swift
+//  KeyboardKitTests
+//
+//  Created by Daniel Saidi on 2019-05-28.
+//  Copyright © 2019 Daniel Saidi. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import KeyboardKit
+
+class UIView_TapActionTests: QuickSpec {
+    
+    override func spec() {
+        
+        describe("adding tap action") {
+            
+            it("adds tap gesture recognizer") {
+                let view = UIView(frame: .zero)
+                expect(view.gestureRecognizers).to(beNil())
+                view.addTapAction {}
+                expect(view.gestureRecognizers?.count).to(equal(1))
+                expect(view.gestureRecognizers?[0] as? UITapGestureRecognizer).toNot(beNil())
+            }
+        }
+        
+        describe("removing tap action") {
+            
+            it("removes correct gesture recognizer") {
+                let view = UIView(frame: .zero)
+                view.addLongPressAction {}
+                view.addTapAction {}
+                expect(view.gestureRecognizers?.count).to(equal(2))
+                view.removeTapAction()
+                expect(view.gestureRecognizers?.count).to(equal(1))
+                expect(view.gestureRecognizers?[0] as? UILongPressGestureRecognizer).toNot(beNil())
+            }
+        }
+    }
+}
