45b5b76a144ed3293b51d6de6f9e7db144996b47
Daniel Saidi <daniel.saidi@gmail.com>
2020-03-11 23:19:34 +0100

    Add parameters to UIImage globe extension

--- Sources/KeyboardKit/UIKit/Extensions/UIImage+Globe.swift
+++ Sources/KeyboardKit/UIKit/Extensions/UIImage+Globe.swift
@@ -13,8 +13,17 @@ public extension UIImage {
     
     /**
      The globe icon that is used for "next keyboard" buttons.
+     
+     You can vary `pointSize` to fit the use-case, then vary
+     `weight` and `scale` to customize it further.
      */
-    static var globe: UIImage? {
-        UIImage(systemName: "globe")
+    static func globe(
+        pointSize: CGFloat,
+        weight: SymbolWeight = .light,
+        scale: SymbolScale = .medium) -> UIImage? {
+        UIImage(systemName: "globe", withConfiguration: SymbolConfiguration(
+            pointSize: pointSize,
+            weight: weight,
+            scale: scale))
     }
 }
