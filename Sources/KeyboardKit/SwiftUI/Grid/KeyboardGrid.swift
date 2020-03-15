026c27cba35882281c53cbfeb26952fae812720e
Daniel Saidi <daniel.saidi@gmail.com>
2020-03-15 22:30:22 +0100

    Remove padding from the grid view

--- Sources/KeyboardKit/SwiftUI/Grid/KeyboardGrid.swift
+++ Sources/KeyboardKit/SwiftUI/Grid/KeyboardGrid.swift
@@ -12,13 +12,10 @@ import SwiftUI
  A `KeyboardGrid` can be used to list actions in a grid with
  a certain number of `columns`.
  
- The grid supports a custom grid `padding` and item `spacing`
- and will even out the provided `actions` array with actions
- so that the array has enough tems to evenly fit the grid.
- 
- The provided `buttonBuilder` will be called for each action
- and will be used to provide the grid with any view for each
- action. This makes the grid very customizable.
+ The grid supports a custom item `spacing` and will fill the
+ provided `actions` array with enough `.none` actions, so it
+ has enough items to evenly fit the grid. The `buttonBuilder`
+ will be used to create a button for each action.
  */
 @available(iOS 13.0, *)
 public struct KeyboardGrid<Button: View>: View {
@@ -26,19 +23,17 @@ public struct KeyboardGrid<Button: View>: View {
     public init(
         actions: [KeyboardAction],
         columns: Int,
-        padding: CGFloat = 10,
         spacing: CGFloat = 10,
         @ViewBuilder buttonBuilder: @escaping (KeyboardAction) -> Button) {
-        self.actions = actions.evened(for: columns)
+        let actions = actions.evened(for: columns)
+        self.actions = actions
         self.rows = actions.batched(withBatchSize: columns)
-        self.padding = padding
         self.spacing = spacing
         self.buttonBuilder = buttonBuilder
     }
     
     public let actions: [KeyboardAction]
     public let rows: KeyboardActionRows
-    public let padding: CGFloat
     public let spacing: CGFloat
     public let buttonBuilder: (KeyboardAction) -> Button
     
@@ -47,7 +42,7 @@ public struct KeyboardGrid<Button: View>: View {
             ForEach(Array(rows.enumerated()), id: \.offset) {
                 self.gridRow(for: $0.element)
             }
-        }.padding(padding)
+        }
     }
 }
 
