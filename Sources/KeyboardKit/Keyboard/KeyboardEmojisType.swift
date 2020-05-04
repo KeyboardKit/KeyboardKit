3f0d6142d7e19179f6e00a7025159c8796b96a0a
Rafael sanchez <sanchezpazarafael@gmail.com>
2020-05-04 18:02:39 -0500

    create a new enum for the emoji categories

---
+++ Sources/KeyboardKit/Keyboard/KeyboardEmojisType.swift
@@ -0,0 +1,16 @@
+import UIKit
+/**
+This enum contains all keyboard types emojis that iphone keyboard currently has
+*/
+public enum KeyboardEmojisType: Equatable {
+
+    case
+    smileys,
+    animals,
+    foods,
+    travels,
+    activities,
+    objects,
+    symbols,
+    flags
+}
