5990f3ac7671418396953c9835ff69ee27982af2
Daniel Saidi <daniel.saidi@gmail.com>
2020-06-29 10:46:20 +0200

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,9 +14,9 @@ let package = Package(
             targets: ["KeyboardKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-        .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.3.0")
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
+        .package(url: "https://github.com/danielsaidi/Mockery.git", .upToNextMajor(from: "0.3.3"))
     ],
     targets: [
         .target(
