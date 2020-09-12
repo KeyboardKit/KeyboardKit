035aeb305b67b72b1829350401ffdf6990ab09b4
Daniel Saidi <daniel.saidi@gmail.com>
2020-09-13 00:07:16 +0200

    Make autocomplete provider and most of the keyboard action handling shared between the two demos

---
+++ KeyboardKitDemo/Shared/DemoKeyboardActionHandlerBase.swift
@@ -0,0 +1,94 @@
+//
+//  DemoAutocompleteSuggestionProvider.swift
+//  KeyboardKitDemo
+//
+//  Created by Daniel Saidi on 2019-07-05.
+//  Copyright © 2019 Daniel Saidi. All rights reserved.
+//
+
+import KeyboardKit
+import UIKit
+
+/**
+ This action handler inherits `StandardKeyboardActionHandler`
+ and adds demo-specific base functionality to it.
+ 
+ The class is shared between all keyboards. Each demo action
+ handler inherit this class and adds functionality on top of
+ the base functionality.
+ */
+class DemoKeyboardActionHandlerBase: StandardKeyboardActionHandler {
+    
+    
+    // MARK: - Initialization
+    
+    init(inputViewController: KeyboardInputViewController) {
+        super.init(
+            inputViewController: inputViewController,
+            hapticConfiguration: .standard
+        )
+    }
+    
+    /**
+     `NOTE` Changing to alphabetic lower case should be done
+     in `StandardKeyboardActionHandler`, not here.
+     */
+    func handleSpace(for sender: Any?) -> GestureAction {
+        let baseAction = super.tapAction(for: .space, sender: sender)
+        return { [weak self] in
+            baseAction?()
+            let type = self?.inputViewController?.context.keyboardType
+            if type?.isAlphabetic == true { return }
+            self?.inputViewController?.changeKeyboardType(to: .alphabetic(.lowercased))
+        }
+    }
+    
+    override func longPressAction(for action: KeyboardAction, sender: Any?) -> GestureAction? {
+        switch action {
+        case .image(_, _, let imageName): return { [weak self] in self?.saveImage(UIImage(named: imageName)!) }
+        default: return super.longPressAction(for: action, sender: sender)
+        }
+    }
+    
+    override func tapAction(for action: KeyboardAction, sender: Any?) -> GestureAction? {
+        switch action {
+        case .image(_, _, let imageName): return { [weak self] in self?.copyImage(UIImage(named: imageName)!) }
+        case .space: return handleSpace(for: sender)
+        default: return super.tapAction(for: action, sender: sender)
+        }
+    }
+    
+    
+    // MARK: - Functions
+    
+    /**
+     Override this function to implement a way to alert text
+     messages in the keyboard extension. You can't use logic
+     that you use in real apps, e.g. `UIAlertController`.
+     */
+    func alert(_ message: String) {
+        assertionFailure("You must override alert")
+    }
+    
+    func copyImage(_ image: UIImage) {
+        guard let input = inputViewController else { return }
+        guard input.hasFullAccess else { return alert("You must enable full access to copy images.") }
+        guard image.copyToPasteboard() else { return alert("The image could not be copied.") }
+        alert("Copied to pasteboard!")
+    }
+    
+    func saveImage(_ image: UIImage) {
+        guard let input = inputViewController else { return }
+        guard input.hasFullAccess else { return alert("You must enable full access to save images to photos.") }
+        let saveCompletion = #selector(handleImage(_:didFinishSavingWithError:contextInfo:))
+        image.saveToPhotos(completionTarget: self, completionSelector: saveCompletion)
+    }
+
+
+    // MARK: - Image Functions
+
+    @objc func handleImage(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if error == nil { alert("Saved!") }
+        else { alert("Failed!") }
+    }
+}
--- KeyboardKitDemoKeyboard/DemoKeyboardActionHandler.swift
+++ KeyboardKitDemoKeyboard/DemoKeyboardActionHandler.swift
@@ -11,56 +11,23 @@ import UIKit
 
 /**
  This action handler inherits `StandardKeyboardActionHandler`
- and adds demo-specific functionality to it.
+ and adds `UIKit` demo-specific functionality to it.
  */
-class DemoKeyboardActionHandler: StandardKeyboardActionHandler {
-    
-    
-    // MARK: - Initialization
-    
-    public init(inputViewController: KeyboardInputViewController) {
-        super.init(
-            inputViewController: inputViewController,
-            hapticConfiguration: .standard
-        )
-    }
+class DemoKeyboardActionHandler: DemoKeyboardActionHandlerBase {
     
     
     // MARK: - Actions
     
-    override func longPressAction(for action: KeyboardAction, sender: Any?) -> GestureAction? {
-        switch action {
-        case .image(_, _, let imageName): return { [weak self] in self?.saveImage(UIImage(named: imageName)!) }
-        default: return super.longPressAction(for: action, sender: sender)
-        }
+    override func alert(_ message: String) {
+        guard let input = inputViewController as? KeyboardViewController else { return }
+        input.alerter.alert(message: message, in: input.view, withDuration: 4)
     }
     
     override func tapAction(for action: KeyboardAction, sender: Any?) -> GestureAction? {
         switch action {
         case .emojiCategory(let cat): return { [weak self] in self?.switchToEmojiKeyboardCategory(cat) }
-        case .image(_, _, let imageName): return { [weak self] in self?.copyImage(UIImage(named: imageName)!) }
-        case .space: return handleSpace(for: sender)
         default: return super.tapAction(for: action, sender: sender)
         }
-    }
-    
-    
-    // MARK: - Action Handling
-    
-    override func handle(_ gesture: KeyboardGesture, on action: KeyboardAction, sender: Any?) {
-        super.handle(gesture, on: action, sender: sender)
-        inputViewController?.performAutocomplete()
-    }
-}
-
-
-// MARK: - Image Functions
-
-@objc extension DemoKeyboardActionHandler {
-    
-    func handleImage(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        if error == nil { alert("Saved!") }
-        else { alert("Failed!") }
     }
 }
 
@@ -68,39 +35,6 @@ class DemoKeyboardActionHandler: StandardKeyboardActionHandler {
 // MARK: - Actions
 
 private extension DemoKeyboardActionHandler {
-    
-    func alert(_ message: String) {
-        guard let input = inputViewController as? KeyboardViewController else { return }
-        input.alerter.alert(message: message, in: input.view, withDuration: 4)
-    }
-    
-    func copyImage(_ image: UIImage) {
-        guard let input = inputViewController as? KeyboardViewController else { return }
-        guard input.hasFullAccess else { return alert("You must enable full access to copy images.") }
-        guard image.copyToPasteboard() else { return alert("The image could not be copied.") }
-        alert("Copied to pasteboard!")
-    }
-    
-    /**
-     `NOTE` Changing to alphabetic lower case should be done
-     in `StandardKeyboardActionHandler`.
-     */
-    func handleSpace(for sender: Any?) -> GestureAction {
-        let baseAction = super.tapAction(for: .space, sender: sender)
-        return { [weak self] in
-            baseAction?()
-            let type = self?.inputViewController?.context.keyboardType
-            if type?.isAlphabetic == true { return }
-            self?.inputViewController?.changeKeyboardType(to: .alphabetic(.lowercased))
-        }
-    }
-    
-    func saveImage(_ image: UIImage) {
-        guard let input = inputViewController as? KeyboardViewController else { return }
-        guard input.hasFullAccess else { return alert("You must enable full access to save images to photos.") }
-        let saveCompletion = #selector(handleImage(_:didFinishSavingWithError:contextInfo:))
-        image.saveToPhotos(completionTarget: self, completionSelector: saveCompletion)
-    }
     
     func switchToEmojiKeyboardCategory(_ cat: EmojiCategory) {
         guard
