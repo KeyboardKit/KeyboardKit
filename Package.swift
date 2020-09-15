22f22517b73ca2b4c190e3dec0d0c0f07817c337
Daniel Saidi <daniel.saidi@gmail.com>
2020-09-15 20:52:49 +0200

    Update Nimble and Mockery

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
-        .package(url: "https://github.com/danielsaidi/Mockery.git", .upToNextMajor(from: "0.6.0"))
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.1.1")),
+        .package(url: "https://github.com/danielsaidi/Mockery.git", .upToNextMajor(from: "0.6.1"))
     ],
     targets: [
         .target(
