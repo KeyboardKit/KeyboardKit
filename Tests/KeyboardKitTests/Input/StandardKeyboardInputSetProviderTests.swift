93db9f6012ddd8fae0e643bb8e577f6c4ed45295
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-01 17:03:39 +0100

    Add more tests for standard input set provider

--- Tests/KeyboardKitTests/Input/StandardKeyboardInputSetProviderTests.swift
+++ Tests/KeyboardKitTests/Input/StandardKeyboardInputSetProviderTests.swift
@@ -17,25 +17,50 @@ class StandardKeyboardInputSetProviderTests: QuickSpec {
         
         describe("standard keyboard input set provider") {
             
-            it("has supported sets for basic English") {
-                let provider = StandardKeyboardInputSetProvider(locale: Locale(identifier: "en"))
-                expect(provider.alphabeticInputSet).to(equal(.alphabetic_en))
-                expect(provider.numericInputSet).to(equal(.numeric_en))
-                expect(provider.symbolicInputSet).to(equal(.symbolic_en))
+            func provider(for identifier: String) -> KeyboardInputSetProvider {
+                StandardKeyboardInputSetProvider(locale: Locale(identifier: identifier))
+            }
+            
+            it("supports English") {
+                let prov = provider(for: "en")
+                expect(prov.alphabeticInputSet).to(equal(.alphabetic_en))
+                expect(prov.numericInputSet).to(equal(.numeric_en))
+                expect(prov.symbolicInputSet).to(equal(.symbolic_en))
+            }
+            
+            it("supports German") {
+                let prov = provider(for: "de")
+                expect(prov.alphabeticInputSet).to(equal(.alphabetic_de))
+                expect(prov.numericInputSet).to(equal(.numeric_de))
+                expect(prov.symbolicInputSet).to(equal(.symbolic_de))
+            }
+            
+            it("supports Italian") {
+                let prov = provider(for: "it")
+                expect(prov.alphabeticInputSet).to(equal(.alphabetic_it))
+                expect(prov.numericInputSet).to(equal(.numeric_it))
+                expect(prov.symbolicInputSet).to(equal(.symbolic_it))
+            }
+            
+            it("supports Swedish") {
+                let prov = provider(for: "sv")
+                expect(prov.alphabeticInputSet).to(equal(.alphabetic_sv))
+                expect(prov.numericInputSet).to(equal(.numeric_sv))
+                expect(prov.symbolicInputSet).to(equal(.symbolic_sv))
             }
             
             it("has fallback support for specific locale") {
-                let provider = StandardKeyboardInputSetProvider(locale: Locale(identifier: "en-US"))
-                expect(provider.alphabeticInputSet).to(equal(.alphabetic_en))
-                expect(provider.numericInputSet).to(equal(.numeric_en))
-                expect(provider.symbolicInputSet).to(equal(.symbolic_en))
+                let prov = provider(for: "en-US")
+                expect(prov.alphabeticInputSet).to(equal(.alphabetic_en))
+                expect(prov.numericInputSet).to(equal(.numeric_en))
+                expect(prov.symbolicInputSet).to(equal(.symbolic_en))
             }
             
             it("has fallback support for non-existing locale") {
-                let provider = StandardKeyboardInputSetProvider(locale: Locale(identifier: "abcd"))
-                expect(provider.alphabeticInputSet).to(equal(.alphabetic_en))
-                expect(provider.numericInputSet).to(equal(.numeric_en))
-                expect(provider.symbolicInputSet).to(equal(.symbolic_en))
+                let prov = provider(for: "abcd")
+                expect(prov.alphabeticInputSet).to(equal(.alphabetic_en))
+                expect(prov.numericInputSet).to(equal(.numeric_en))
+                expect(prov.symbolicInputSet).to(equal(.symbolic_en))
             }
         }
     }
