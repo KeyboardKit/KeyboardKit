33c5b301ea2e327ef8ae02563b8057c9c2a89542
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-28 09:00:09 +0100

    Adjust KeyboardActionHandler documentation

--- Sources/KeyboardKit/Actions/KeyboardActionHandler.swift
+++ Sources/KeyboardKit/Actions/KeyboardActionHandler.swift
@@ -12,10 +12,15 @@ import UIKit
  This protocol can be implemented by classes that can handle
  keyboard actions.
  
- KeyboardKit registers a standard protocol implementation in
- the input view controller's `context` when the extension is
- started. You can replace this by registering a new instance
- with the `actionHandler` property.
+ KeyboardKit will registers a `StandardKeyboardActionHandler`
+ in the input view controller's `context` when the extension
+ is started. You can replace the standard one by registering
+ a new `actionHandler` in this context.
+ 
+ `StandardKeyboardActionHandler` has a LOT of built-in logic
+ that can also be overridden. If you want to create a custom
+ action handler, you should probably subclass it and replace
+ the parts you want to customize.
  */
 public protocol KeyboardActionHandler: AnyObject {
     
