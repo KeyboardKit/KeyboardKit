228d4d404c564eeb8f25cbff98fa9bf89f07c09c
Daniel Saidi <daniel.saidi@gmail.com>
2021-01-10 10:29:32 +0100

    Make demo keyboard appearance provider change text and font for space

--- KeyboardKitDemo/Shared/DemoKeyboardAppearanceProvider.swift
+++ KeyboardKitDemo/Shared/DemoKeyboardAppearanceProvider.swift
@@ -7,6 +7,7 @@
 //
 
 import KeyboardKit
+import UIKit
 
 /**
  This provider inherits `StandardKeyboardAppearanceProvider`
@@ -14,10 +15,17 @@ import KeyboardKit
  */
 class DemoKeyboardAppearanceProvider: StandardKeyboardAppearanceProvider {
     
-    override func text(for action: KeyboardAction, context: KeyboardContext) -> String? {
+    override func font(for action: KeyboardAction) -> UIFont {
+        switch action {
+        case .space: return .preferredFont(forTextStyle: .body)
+        default: return super.font(for: action)
+        }
+    }
+    
+    override func text(for action: KeyboardAction) -> String? {
         switch action {
         case .space: return "space"
-        default: return super.text(for: action, context: context)
+        default: return super.text(for: action)
         }
     }
 }
