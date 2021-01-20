7d5117ce76f12960cfcdff5c4aaed3f7fc843f1e
Daniel Saidi <daniel.saidi@gmail.com>
2021-01-20 14:48:04 +0100

    Update MockingKit

--- Sources/KeyboardKit/Actions/KeyboardAction+Actions.swift
+++ Sources/KeyboardKit/Actions/KeyboardAction+Actions.swift
@@ -33,7 +33,6 @@ public extension KeyboardAction {
     var standardLongPressAction: GestureAction? {
         switch self {
         case .backspace: return standardTapAction
-        // case .shift: return { $0?.changeKeyboardType(to: .alphabetic(.capsLocked)) }
         default: return nil
         }
     }
