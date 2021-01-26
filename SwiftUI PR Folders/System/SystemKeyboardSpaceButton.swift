23375159d1a3e6fe673d0fc43f2b94dbd7f60fd9
Brennan Drew <nannerb156@gmail.com>
2021-01-26 10:45:51 -0800

    Adding Layout and System folders from KeyboardKitSwiftUI repo
    (cherry picked from commit da33b26e850112a5d2587aee828fad958c437f9e)

---
+++ SwiftUI PR Folders/System/SystemKeyboardSpaceButton.swift
@@ -0,0 +1,37 @@
+//
+//  SystemKeyboardSpaceButton.swift
+//  KeyboardKit
+//
+//  Created by Daniel Saidi on 2021-01-10.
+//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//
+
+import KeyboardKit
+import SwiftUI
+
+/**
+ This view mimics the system space button, which starts with
+ the provided `localeText`, then fades in `spaceText`.
+ 
+ This view renders a `SystemKeyboardSpaceButtonContent` then
+ applies a standard button style and action gestures to it.
+ */
+public struct SystemKeyboardSpaceButton: View {
+    
+    public init(localeText: String, spaceText: String) {
+        self.localeText = localeText
+        self.spaceText = spaceText
+    }
+    
+    private let localeText: String
+    private let spaceText: String
+    private var action: KeyboardAction { .space }
+    
+    @EnvironmentObject var context: ObservableKeyboardContext
+    
+    public var body: some View {
+        SystemKeyboardSpaceButtonContent(localeText: localeText, spaceText: spaceText)
+            .standardButtonStyle(for: action, context: context)
+            .keyboardAction(action, actionHandler: context.actionHandler)
+    }
+}
