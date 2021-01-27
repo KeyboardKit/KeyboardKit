449a6cbda0bfaed7194ffa2a1e0eec936c4cdfa0
Daniel Saidi <daniel.saidi@gmail.com>
2021-01-27 17:32:11 +0100

    Remove remaining deprecations

--- Sources/KeyboardKit/_Deprecated/KeyboardType+Deprecations.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-public extension KeyboardType {
-    
-    @available(*, deprecated, renamed: "standardButtonImage")
-    func systemKeyboardButtonImage(for context: KeyboardContext) -> Image? {
-        standardButtonImage
-    }
-}
