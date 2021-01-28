c0b190306fcb29ed0daaedb86b9f94f85d31e31e
Daniel Saidi <daniel.saidi@gmail.com>
2021-01-28 14:15:06 +0100

    Add a preview context

---
+++ Sources/KeyboardKit/Previews/ObservableKeyboardContext+Preview.swift
@@ -0,0 +1,23 @@
+//
+//  ObservableKeyboardContext+Preview.swift
+//  KeyboardKit
+//
+//  Created by Daniel Saidi on 2021-01-28.
+//  Copyright © 2021 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+public extension ObservableKeyboardContext {
+    
+    /**
+     This preview class can be used when previewing keyboard
+     views that depend on an `ObservableKeyboardContext`. Do
+     not use otherwise.
+     */
+    static var preview: ObservableKeyboardContext {
+        ObservableKeyboardContext(
+            controller: KeyboardInputViewController(),
+            actionHandler: FakeKeyboardActionHandler())
+    }
+}
