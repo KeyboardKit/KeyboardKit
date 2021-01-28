773b2d97647357d4af328e1b90b3c41e385e1b79
Daniel Saidi <daniel.saidi@gmail.com>
2021-01-28 20:29:57 +0100

    Change copyright information

--- Sources/KeyboardKit/Extensions/View+Frame.swift
+++ Sources/KeyboardKit/Extensions/View+Frame.swift
@@ -3,7 +3,7 @@
 //  KeyboardKit
 //
 //  Created by Daniel Saidi on 2021-01-08.
-//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//  Copyright © 2021 Daniel Saidi. All rights reserved.
 //
 
 import SwiftUI
--- Tests/KeyboardKitTests/Keyboard/KeyboardHostingControllerTests.swift
+++ Tests/KeyboardKitTests/Keyboard/KeyboardHostingControllerTests.swift
@@ -3,7 +3,7 @@
 //  KeyboardKitTests
 //
 //  Created by Daniel Saidi on 2020-06-15.
-//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//  Copyright © 2021 Daniel Saidi. All rights reserved.
 //
 
 import Quick
