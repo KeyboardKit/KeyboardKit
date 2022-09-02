735493ef995a708727a294f2b0555fe982090ec7
Daniel Saidi <daniel.saidi@gmail.com>
2022-09-02 09:27:23 +0200

    Update the fake autocomplete provider

--- Demo/Demo/Keyboard/FakeAutocompleteProvider.swift
+++ Demo/Demo/Keyboard/FakeAutocompleteProvider.swift
@@ -10,11 +10,25 @@ import Foundation
 import KeyboardKit
 
 /**
- This fake provider simply returns the current word suffixed
- with "ly", "er" and "ter". It adds a subtitle to the middle
- suggestion as well, to show how subtitles can be used.
+ This autocomplete provider always returns three suggestions,
+ with the current word suffixed with "-1", "-2" and "-3". It
+ also adds a static subtitle to the middle suggestion.
+
+ If the currently typed word is "match" (this can be changed
+ in the init) the provider returns another three suggestions.
+ The left is an "unknown" suggestion, which is surrounded by
+ quotes. The center is an autocomplete suggestion, which has
+ a semi-white background and is automatically applied if the
+ user types a word delimiter. The right is a regular, boring
+ suggestion without any special look or behavior.
  */
 class FakeAutocompleteProvider: AutocompleteProvider {
+
+    init(match: String = "match") {
+        self.match = match
+    }
+
+    private var match: String
     
     var locale: Locale = .current
     
@@ -32,21 +46,33 @@ class FakeAutocompleteProvider: AutocompleteProvider {
     
     func autocompleteSuggestions(for text: String, completion: AutocompleteCompletion) {
         guard text.count > 0 else { return completion(.success([])) }
-        completion(.success(suggestions(for: text)))
+        if text == match {
+            completion(.success(matchSuggestions()))
+        } else {
+            completion(.success(fakeSuggestions(for: text)))
+        }
     }
 }
 
 private extension FakeAutocompleteProvider {
     
-    func suggestions(for text: String) -> [AutocompleteSuggestion] {
+    func fakeSuggestions(for text: String) -> [AutocompleteSuggestion] {
         [
-            suggestion(text + "ly"),
-            suggestion(text + "er", "Subtitle"),
-            suggestion(text + "ter")
+            StandardAutocompleteSuggestion(text: text + "-1"),
+            StandardAutocompleteSuggestion(text: text + "-2", subtitle: "Subtitle"),
+            StandardAutocompleteSuggestion(text: text + "-3")
         ]
     }
     
-    func suggestion(_ word: String, _ subtitle: String? = nil) -> AutocompleteSuggestion {
-        StandardAutocompleteSuggestion(text: word, title: word, subtitle: subtitle)
+    func fakeSuggestion(_ text: String, _ subtitle: String? = nil) -> AutocompleteSuggestion {
+        StandardAutocompleteSuggestion(text: text, subtitle: subtitle)
+    }
+
+    func matchSuggestions() -> [AutocompleteSuggestion] {
+        [
+            StandardAutocompleteSuggestion(text: match, isUnknown: true),
+            StandardAutocompleteSuggestion(text: match, isAutocomplete: true),
+            StandardAutocompleteSuggestion(text: match),
+        ]
     }
 }
