79d25d638259cced696752619c2951964885d855
Daniel Saidi <daniel.saidi@gmail.com>
2022-12-22 10:16:42 +0100

    Fix unit tests

--- Tests/KeyboardKitTests/Actions/KeyboardActionTests.swift
+++ Tests/KeyboardKitTests/Actions/KeyboardActionTests.swift
@@ -71,7 +71,7 @@ final class KeyboardActionTests: XCTestCase {
 
     func testIsSpacerIsTrueForSomeActions() {
         actions.forEach { action in
-            let result = action.isInputAction
+            let result = action.isSpacer
             switch action {
             case .characterMargin(""): XCTAssertTrue(result)
             case .none: XCTAssertTrue(result)
