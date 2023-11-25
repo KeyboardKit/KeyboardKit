fadce2d4405a718960b3e7ca4b744da72ae95c41
Daniel Saidi <daniel.saidi@gmail.com>
2023-11-25 14:40:40 +0100

    Move keyboard locale flag extensions to separate file

---
+++ Sources/KeyboardKit/Localization/KeyboardLocale+Flag.swift
@@ -0,0 +1,105 @@
+//
+//  Locale+Flag.swift
+//  KeyboardKit
+//
+//  Created by Daniel Saidi on 2022-10-27.
+//  Copyright © 2022-2023 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public extension KeyboardLocale {
+    
+    /// The corresponding flag emoji for the locale.
+    var flag: String {
+        switch self {
+        case .albanian: return "🇦🇱"
+        case .arabic: return "🇦🇪"
+        case .armenian: return "🇦🇲"
+        case .belarusian: return "🇧🇾"
+        case .bulgarian: return "🇧🇬"
+        case .catalan: return "🇦🇩"
+        case .cherokee: return "🏳️"
+        case .croatian: return "🇭🇷"
+        case .czech: return "🇨🇿"
+        case .danish: return "🇩🇰"
+            
+        case .dutch: return "🇳🇱"
+        case .dutch_belgium: return "🇧🇪"
+        case .english: return "🇺🇸"
+        case .english_gb: return "🇬🇧"
+        case .english_us: return "🇺🇸"
+        case .estonian: return "🇪🇪"
+        case .faroese: return "🇫🇴"
+        case .filipino: return "🇵🇭"
+        case .finnish: return "🇫🇮"
+        case .french: return "🇫🇷"
+            
+        case .french_belgium: return "🇧🇪"
+        case .french_switzerland: return "🇨🇭"
+        case .georgian: return "🇬🇪"
+        case .german: return "🇩🇪"
+        case .german_austria: return "🇦🇹"
+        case .german_switzerland: return "🇨🇭"
+        case .greek: return "🇬🇷"
+        case .hawaiian: return "🇺🇸"
+        case .hebrew: return "🇮🇱"
+        case .hungarian: return "🇭🇺"
+            
+        case .icelandic: return "🇮🇸"
+        case .indonesian: return "🇮🇩"
+        case .irish: return "🇮🇪"
+        case .italian: return "🇮🇹"
+        case .kazakh: return "🇰🇿"
+        case .kurdish_sorani: return "🇹🇯"
+        case .kurdish_sorani_arabic: return "🇹🇯"
+        case .kurdish_sorani_pc: return "🇹🇯"
+        case .latvian: return "🇱🇻"
+        case .lithuanian: return "🇱🇹"
+        case .macedonian: return "🇲🇰"
+            
+        case .malay: return "🇲🇾"
+        case .maltese: return "🇲🇹"
+        case .mongolian: return "🇲🇳"
+        case .norwegian: return "🇳🇴"
+        case .persian: return "🇮🇷"
+        case .polish: return "🇵🇱"
+        case .portuguese: return "🇵🇹"
+        case .portuguese_brazil: return "🇧🇷"
+        case .romanian: return "🇷🇴"
+        case .russian: return "🇷🇺"
+            
+        case .serbian: return "🇷🇸"
+        case .serbian_latin: return "🇷🇸"
+        case .slovenian: return "🇸🇮"
+        case .slovak: return "🇸🇰"
+        case .spanish: return "🇪🇸"
+        case .swedish: return "🇸🇪"
+        case .swahili: return "🇰🇪"
+        case .turkish: return "🇹🇷"
+        case .ukrainian: return "🇺🇦"
+        case .uzbek: return "🇺🇿"
+        }
+    }
+}
+
+@available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
+public extension Locale {
+
+    /**
+     Get the locale flag symbol that can be used as an emoji.
+
+     This only works if the locale has a region identifier.
+     */
+    var flag: String? {
+        let regionIdentifier = region?.identifier
+        let flagBase = UnicodeScalar("🇦").value - UnicodeScalar("A").value
+        let flag = regionIdentifier?
+            .uppercased()
+            .unicodeScalars
+            .compactMap { UnicodeScalar(flagBase + $0.value)?.description }
+            .joined()
+        return flag ?? ""
+    }
+}
---
+++ Tests/KeyboardKitTests/Localization/KeyboardLocale+FlagTests.swift
@@ -0,0 +1,97 @@
+//
+//  KeyboardLocale+FlagTests.swift
+//  KeyboardKit
+//
+//  Created by Daniel Saidi on 2021-03-17.
+//  Copyright © 2021-2023 Daniel Saidi. All rights reserved.
+//
+
+import KeyboardKit
+import XCTest
+
+class KeyboardLocale_FlagTests: XCTestCase {
+    
+    func testFlagIsValidForAllCases() {
+        let map = KeyboardLocale.all.map { ($0, $0.flag) }
+        let result = Dictionary(uniqueKeysWithValues: map)
+        let expected: [KeyboardLocale: String] = [
+            .albanian: "🇦🇱",
+            .arabic: "🇦🇪",
+            .armenian: "🇦🇲",
+            .belarusian: "🇧🇾",
+            .bulgarian: "🇧🇬",
+            .catalan: "🇦🇩",
+            .cherokee: "🏳️",
+            .croatian: "🇭🇷",
+            .czech: "🇨🇿",
+            .danish: "🇩🇰",
+            .dutch: "🇳🇱",
+            .dutch_belgium: "🇧🇪",
+            .english: "🇺🇸",
+            .english_gb: "🇬🇧",
+            .english_us: "🇺🇸",
+            .estonian: "🇪🇪",
+            .faroese: "🇫🇴",
+            .filipino: "🇵🇭",
+            .finnish: "🇫🇮",
+            .french: "🇫🇷",
+            .french_belgium: "🇧🇪",
+            .french_switzerland: "🇨🇭",
+            .georgian: "🇬🇪",
+            .german: "🇩🇪",
+            .german_austria: "🇦🇹",
+            .german_switzerland: "🇨🇭",
+            .greek: "🇬🇷",
+            .hawaiian: "🇺🇸",
+            .hebrew: "🇮🇱",
+            .hungarian: "🇭🇺",
+            .icelandic: "🇮🇸",
+            .indonesian: "🇮🇩",
+            .irish: "🇮🇪",
+            .italian: "🇮🇹",
+            .kazakh: "🇰🇿",
+            .kurdish_sorani: "🇹🇯",
+            .kurdish_sorani_arabic: "🇹🇯",
+            .kurdish_sorani_pc: "🇹🇯",
+            .latvian: "🇱🇻",
+            .lithuanian: "🇱🇹",
+            .macedonian: "🇲🇰",
+            .malay: "🇲🇾",
+            .maltese: "🇲🇹",
+            .mongolian: "🇲🇳",
+            .norwegian: "🇳🇴",
+            .persian: "🇮🇷",
+            .polish: "🇵🇱",
+            .portuguese: "🇵🇹",
+            .portuguese_brazil: "🇧🇷",
+            .romanian: "🇷🇴",
+            .russian: "🇷🇺",
+            .serbian: "🇷🇸",
+            .serbian_latin: "🇷🇸",
+            .slovenian: "🇸🇮",
+            .slovak: "🇸🇰",
+            .spanish: "🇪🇸",
+            .swedish: "🇸🇪",
+            .swahili: "🇰🇪",
+            .turkish: "🇹🇷",
+            .ukrainian: "🇺🇦",
+            .uzbek: "🇺🇿"
+        ]
+
+        XCTAssertEqual(result.keys, expected.keys)
+        result.keys.forEach {
+            XCTAssertEqual(result[$0], expected[$0])
+        }
+    }
+
+    @available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
+    func testFlags() {
+        let doPrint = false
+        KeyboardLocale.allCases.forEach { locale in
+            let isEqual = locale.flag == locale.locale.flag
+            if !isEqual && doPrint {
+                print("*** \(locale.locale.localizedName): \(locale.flag) vs \(locale.locale.flag ?? "-")")
+            }
+        }
+    }
+}
