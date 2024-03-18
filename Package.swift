1848699a09cd749ef2ce3f92284f6a3897202333
Daniel Saidi <daniel.saidi@gmail.com>
2024-03-18 10:05:27 +0100

    Add support for visionOS

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .iOS(.v14),
         .macOS(.v11),
         .tvOS(.v14),
-        .watchOS(.v7)
+        .watchOS(.v7),
+        .visionOS(.v1)
     ],
     products: [
         .library(
@@ -35,4 +36,3 @@ let package = Package(
         )
     ]
 )
-    
