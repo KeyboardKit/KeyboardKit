889ce2bcb2df4395effe0271f40f84aa20966623
Daniel Saidi <daniel.saidi@gmail.com>
2025-01-24 15:34:57 +0100

    Add enabled locales properties to the keyboard context

--- Sources/KeyboardKit/_Essentials/KeyboardContext+Locales.swift
+++ Sources/KeyboardKit/_Essentials/KeyboardContext+Locales.swift
@@ -9,24 +9,33 @@
 import Foundation
 
 public extension KeyboardContext {
+    
+    /// All ``Keyboard/Settings/addedLocales`` or ``locales``.
+    var enabledLocales: [Locale] {
+        let addedLocales = settings.addedLocales
+        return addedLocales.isEmpty ? locales : addedLocales
+    }
+    
+    /// Whether the context has multiple enabled locales.
+    var hasMultipleEnabledLocales: Bool {
+        enabledLocales.count > 1
+    }
+    
+    /// Whether the space bar should apply a trailing locale
+    /// context menu.
+    var shouldAddLocaleContextMenuToSpaceBar: Bool {
+        guard hasMultipleEnabledLocales else { return false }
+        return spaceLongPressBehavior.shouldAddTrailingLocaleContextMenu
+    }
 
     /// Select the next locale in the selectable locales.
     func selectNextLocale() {
-        let locales = selectableLocales
+        let locales = enabledLocales
         let fallback = locales.first ?? locale
         let firstIndex = locales.firstIndex(of: locale)
         guard let index = firstIndex else { return locale = fallback }
         let nextIndex = index.advanced(by: 1)
         guard locales.count > nextIndex else { return locale = fallback }
         locale = locales[nextIndex]
-    }
-}
-
-extension KeyboardContext {
-
-    /// This is internal until we find a better name for it.
-    var selectableLocales: [Locale] {
-        let addedLocales = settings.addedLocales
-        return addedLocales.isEmpty ? locales : addedLocales
     }
 }
