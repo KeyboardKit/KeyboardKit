6d5757ae211e539a5dd21a9a84222ac532dd7e43
Daniel Saidi <daniel.saidi@gmail.com>
2025-08-31 06:21:12 +0200

    Update deployment targets

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ let package = Package(
     name: "KeyboardKit",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8),
+        .iOS(.v16),
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v10),
         .visionOS(.v1)
     ],
     products: [
