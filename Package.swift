438f4b285a8507a230706ff22f2a675e8bbbbf40
Daniel Saidi <daniel.saidi@gmail.com>
2025-09-27 18:56:01 +0200

    Update package for release candidate update

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KeyboardKit",
-            url: "https://github.com/KeyboardKit/KeyboardKit-Binaries/releases/download/10.0.0-rc.1/KeyboardKit.zip",
-            checksum: "e85c9babd4fc1e1895a4658ca5ff7696c11ccc8fea3938f95534946cb43e0138"
+            url: "https://github.com/KeyboardKit/KeyboardKit-Binaries/releases/download/10.0.0-rc.1.b2/KeyboardKit.zip",
+            checksum: "d08365678e425ebc499a78936d99f2b65f9c478b05a543282e5ef72ec88a2bfb"
         )
     ]
 )
